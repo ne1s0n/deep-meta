1d5b443a1f32ec312fbf83a3f587cf2c5d2f14bd
ne1s0n <nelson.nazzicari@gmail.com>
2020-10-15 17:43:06 +0200

    added function to clone Yıldırım et al Arrhythmia model

--- DM/model_tools.py
+++ DM/model_tools.py
@@ -1,6 +1,6 @@
 from keras.optimizers import Adam
 from keras.models import Sequential, Model
-from keras.layers import Dense, Conv1D, Flatten, MaxPooling1D, Dropout, Input
+from keras.layers import Dense, Conv1D, Flatten, MaxPooling1D, Dropout, Input, BatchNormalization
 import keras.metrics
 
 def get_model_seq1(conv_filters, conv_kernel_sizes, dropout_rates, pool_sizes, input_shape, output_units, lr):
@@ -23,6 +23,56 @@ def get_model_seq1(conv_filters, conv_kernel_sizes, dropout_rates, pool_sizes, i
 
 	#top layers
 	model.add(Flatten())
+	#binary or multiclass?
+	if output_units > 2:
+		model.add(Dense(units = output_units, activation='softmax'))
+		target_loss = 'categorical_crossentropy'
+		target_accuracy = ['categorical_accuracy']
+	else:
+		model.add(Dense(units = 1, activation='sigmoid'))
+		target_loss = 'binary_crossentropy'
+		target_accuracy = ['binary_accuracy']
+
+	#the model is declared, but we still need to compile it to actually
+	#build all the data structures
+	model.compile(optimizer = Adam(learning_rate = lr),
+		loss = target_loss, 
+		metrics = target_accuracy)
+
+	return(model) 
+
+def get_model_Yıldırım(input_shape, output_units, lr):
+	"""Model from:
+		Yıldırım, Özal, et al. 
+		"Arrhythmia detection using deep convolutional neural network with long duration ECG signals." 
+		Computers in biology and medicine 102 (2018): 411-420.
+	"""
+	#declare a base model
+	model = Sequential()
+
+	#let's start with the input layer
+	model.add(Input(shape = input_shape))
+	
+	#adding the computing layers
+	model.add(Conv1D(filters = 128, activation='relu', kernel_size = 50, strides = 3, padding = 'same'))
+	model.add(BatchNormalization())	
+	model.add(MaxPooling1D(pool_size=2, strides=3, padding='same'))
+	model.add(Conv1D(filters = 32, activation='relu', kernel_size = 7, strides = 1, padding = 'same'))
+	model.add(BatchNormalization())	
+	model.add(MaxPooling1D(pool_size=2, strides=2, padding='same'))
+	model.add(Conv1D(filters = 32, activation='relu', kernel_size = 10, strides = 1, padding = 'same'))
+	model.add(Conv1D(filters = 128, activation='relu', kernel_size = 5, strides = 2, padding = 'same'))
+	model.add(MaxPooling1D(pool_size=2, strides=2, padding='same'))
+	model.add(Conv1D(filters = 256, activation='relu', kernel_size = 15, strides = 1, padding = 'same'))
+	model.add(MaxPooling1D(pool_size=2, strides=2, padding='same'))
+	model.add(Conv1D(filters = 512, activation='relu', kernel_size = 5, strides = 1, padding = 'same'))
+	model.add(Conv1D(filters = 128, activation='relu', kernel_size = 3, strides = 1, padding = 'same'))
+	
+	#top layers
+	model.add(Flatten())
+	model.add(Dense(units = 512, activation='relu'))
+	model.add(Dropout(rate = 0.1))
+	
 	#binary or multiclass?
 	if output_units > 2:
 		model.add(Dense(units = output_units, activation='softmax'))
